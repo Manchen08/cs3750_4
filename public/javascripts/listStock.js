c34ddb93bc3d2400c612746b04a5fcd6a93a5f90
bobbyloock <bobby.loock@gmail.com>
2017-04-19 18:51:41 -0600

    css stuff, moved footer, changed size of button, etc.

--- public/javascripts/listStock.js
+++ public/javascripts/listStock.js
@@ -7,5 +7,27 @@ user.findone({username:req.user.username}, function(err,user){
 });
 
 
+/*
+router.post('/deleteStock', function(req, res, next) {
+var stock = {
+        fullname: req.body.fullname,
+        stock: req.body.stock,
+        percent: req.body.percent
+    };
 
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        db.collection('userSchema').find({fullname: stock.fullname, stock: stock.stock, percent: stock.percent}).count(function(error, result) {
+            if (result != 0 && error == null) {
+                db.collection('userSchema').deleteOne(fullname, stock, percent, function(err, result) {
+                    assert.equal(null, err);
+                    console.log('Stock Deleted');
+                    db.close();
+                });
+            }
+        });
+    });
+    res.redirect('/listStock');
+});
+*/
 
